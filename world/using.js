0c809872d953cd76ac483ef49f004244ef42fb86
Jim MacArthur <jim-github@mode7.co.uk>
2014-10-19 14:25:08 +0100

    Create using.js

---
+++ world/using.js
@@ -0,0 +1,5 @@
+command('use:paint', 'Do something with a thing', function (itemName, player, game) {
+  var rest = item.name;
+  player.write("You don't know how to paint things.");
+  preventDefault();
+});
