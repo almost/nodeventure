5ec16973306312100f052b883e0565bd33f7b56b
bckorolev <timcorless@gmail.com>
2014-10-19 14:18:04 +0100

    Update tenforward.js

--- world/tenforward.js
+++ world/tenforward.js
@@ -5,7 +5,7 @@ exits: { north: 'sickbay' },
 items: [
    {
     name: 'prune juice',
-    short: 'Worf\'s prune juice,
+    short: 'Worf\'s prune juice',
     description: 'A nice glass of prune juice',
     respawnTimer: 60
   }
