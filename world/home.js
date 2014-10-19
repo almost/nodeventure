545de4e507e8c3d2811ce1dbfbbb481a17321b8b
Jim MacArthur <jim-github@mode7.co.uk>
2014-10-19 14:14:01 +0100

    Re-add edit lost in merge

--- world/home.js
+++ world/home.js
@@ -1,6 +1,6 @@
 room('home', {
   description: "You are in the captains quarters room at BarCamp Machester 5. Where to now?",
-  exits: { west: 'beige' },
+  exits: { west: 'beige', east: 'tenforward' },
   items: [
     {
       name: 'door',
