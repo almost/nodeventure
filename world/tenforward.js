ece1958e240624ed704c67a80f0021efe696a716
bckorolev <timcorless@gmail.com>
2014-10-19 14:17:19 +0100

    Update tenforward.js
    Added item

--- world/tenforward.js
+++ world/tenforward.js
@@ -3,5 +3,11 @@ room('tenfoward', {
 description: "You are in Ten Forward. Please drink responsibly",
 exits: { north: 'sickbay' },
 items: [
+   {
+    name: 'prune juice',
+    short: 'Worf\'s prune juice,
+    description: 'A nice glass of prune juice',
+    respawnTimer: 60
+  }
 ]
 });
