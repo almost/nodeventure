ba230cb3208c7a294a173012a1a1732412147542
mark <mfrimston@gmail.com>
2014-10-19 14:06:37 +0100

    added marks room

--- world/home.js
+++ world/home.js
@@ -1,6 +1,7 @@
 room('home', {
   description: "You are in the captains quarters room at BarCamp Machester 5. Where to now?",
-  exits: { west: 'beige' },
+  exits: { west: 'beige', south: 'marksroom' },
   items: [
+  	'rubber chicken with a pulley in the middle'
   ]
 });
---
+++ world/marksroom.js
@@ -0,0 +1,12 @@
+room('marksroom', {
+  description: "You find yourself in an immaculately-decorated Louis XVI bedroom. The room is lit by illuminated floor tiles. In the centre of the room stands a tall, black monolith",
+  //image: null,
+  exits: { },
+  items: [
+    {
+      name: 'monolith',
+      short: 'a monolith',
+      description: 'A looming, black monolith, 1ft x 4ft x 9ft',
+    }
+  ]
+});
