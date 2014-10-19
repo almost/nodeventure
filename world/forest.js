357b414547753b68579e4fe65e3c0b26bae2db54
badspyro <badspyro@gmail.com>
2014-10-19 14:29:58 +0100

    Update forest.js

--- world/forest.js
+++ world/forest.js
@@ -1,1 +1,4 @@
-// Blank file
+// forest entry
+
+room ('forest', {
+  description: "The emerald forest streches out before you, with the shafts of delicate green light dancing through the leaves. a"
