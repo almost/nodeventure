4e9d6574097fb20d6a260bb6f5e1f5c8faeca5c1
mark <mfrimston@gmail.com>
2014-10-19 14:24:32 +0100

    stuff

--- world/marksroom.js
+++ world/marksroom.js
@@ -1,12 +1,20 @@
 room('marksroom', {
   description: "You find yourself in an immaculately-decorated Louis XVI bedroom. The room is lit by illuminated floor tiles. In the centre of the room stands a tall, black monolith",
-  //image: null,
-  exits: { },
+  exits: { 
+  	north: 'home'
+  },
   items: [
     {
       name: 'monolith',
       short: 'a monolith',
       description: 'A looming, black monolith, 1ft x 4ft x 9ft',
+      gettable: false
     }
   ]
 });
+
+for(name in ["use","push","take","pick up"]){
+	handler(name+":monolith",function(game,player,item){
+		player.broadcast("The monolith falls over, unimpressively.");
+	});
+}
