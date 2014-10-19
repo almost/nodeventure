d18c0629d214d5bb87161680c423f4d656ed1116
Jim MacArthur <jim-github@mode7.co.uk>
2014-10-19 14:13:36 +0100

    Add door

--- world/home.js
+++ world/home.js
@@ -1,6 +1,12 @@
 room('home', {
   description: "You are in the captains quarters room at BarCamp Machester 5. Where to now?",
-  exits: { west: 'beige', east: 'tenforward' },
+  exits: { west: 'beige' },
   items: [
+    {
+      name: 'door',
+      short: 'a locked wooden door',
+      description: 'There is an intriguing wooden door with no handle on the north wall',
+      gettable : false
+    }
   ]
 });
