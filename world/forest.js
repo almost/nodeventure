06a18c86e5de3c91a4e3cc9301fc4e0620164c71
badspyro <badspyro@gmail.com>
2014-10-21 12:05:06 +0100

    Update forest.js

--- world/forest.js
+++ world/forest.js
@@ -1,5 +1,6 @@
 // forest entry
 
 room ('forest', {
-  description: "The emerald forest streches out before you, with the shafts of delicate green light dancing through the leaves. a"
+  description: "The emerald forest streches out before you, with the shafts of delicate green light dancing through the leaves. A path has been worn into the forrest floor over many years, and it meanders to the south. To the north, hewn into the rockface, is a door.",
+  exits: {north: }
 });
